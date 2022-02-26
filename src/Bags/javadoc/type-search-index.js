479705d9b21ce8c98da2df6a1e9085586274d6e8
Jimmy Peng <jcpeng@dinlow.com>
2022-02-26 14:38:38 -0800

    old javadoc deleted

--- src/Bags/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"Bags","l":"bagDriver"},{"p":"Bags","l":"BagInterface"},{"p":"Bags","l":"LinkedBag"},{"p":"Bags","l":"ResizableArrayBag"}]
