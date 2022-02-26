77e4193bf57b6e3607317fbcff8f69a4818a899e
kyuhmi <kyunghomin@cpp.edu>
2022-02-26 15:19:07 -0800

    removed an unused import

--- tests/LinkedBagTest.java
+++ tests/LinkedBagTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import Bags.LinkedBag;
-import Bags.ResizableArrayBag;
 
 public class LinkedBagTest {
     private LinkedBag<String> test;
